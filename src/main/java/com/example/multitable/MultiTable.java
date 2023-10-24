edd603962b22d730a104395c853741c47fb7d8ac
jjems <jjems102@gmail.com>
2023-10-24 15:05:46 +0900

    feat: if문을 이용해 가로/세로로 찍기

--- src/main/java/com/example/multitable/MultiTable.java
+++ src/main/java/com/example/multitable/MultiTable.java
@@ -1,15 +1,35 @@
 package com.example.multitable;
 
+import java.util.Scanner;
+
 public class MultiTable {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 2; i <= 9; i++) {
-            System.out.print(i + "단 : " + "\t");
+        System.out.println("가로출력은 1/ 세로출력은 2를 입력:");
+        int choice = scanner.nextInt();
 
-            for (int j = 1; j <= 9; j++) {
-                System.out.print(i + "X" + j + "=" + i * j + "\t");
+        if (choice == 1) {
+
+            for (int i = 2; i <= 9; i++) {
+                System.out.print(i + "단 : " + "\t");
+
+                for (int j = 1; j <= 9; j++) {
+                    System.out.print(i + "X" + j + "=" + i * j + "\t");
+                }
+                System.out.println("");
             }
-            System.out.println("");
+        } else if (choice == 2) {
+            for (int i = 2; i <= 9; i++) {
+                System.out.println(i + "단 : " + "\n");
+
+                for (int j = 1; j <= 9; j++) {
+                    System.out.println(i + "X" + j + "=" + i * j );
+                }
+                System.out.println("");
+            }
+        }else{
+            System.out.print("댓츠 ㄴㄴ");
         }
     }
 }
